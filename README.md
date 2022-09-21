5b4fef31d71df2e408948a2cf3d85c6a53ee33b5
yym68686 <yym68686@outlook.com>
2022-09-21 19:57:01 +0800

    complete Task 3

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ python task-2-RK-ARWU\kr.py
 json 在线转换 excel
 
 [在线JSON转Excel工具 - UU在线工具 (wejson.cn)](https://wejson.cn/json2excel/)
+
+# Task 3
+
+下载 软科中国大学专业排名 bcsr
+
+在浏览器开发者工具里网络中查找 http://127.0.0.1:8651，后面跟的 url 就是 API 接口。
+
+中国大学专业排名 API 接口
+
+https://www.shanghairanking.cn/api/pub/v1/bcsr/rank?target_yr=2022&subj_code=0101
+
+学科代码 API 接口
+
+https://www.shanghairanking.cn/api/pub/v1/bcsr/subj?year=2022
---
+++ task-3-bcsr/bcsr.py
@@ -0,0 +1,46 @@
+import os
+import json
+import requests
+# import pandas as pd
+
+dirpath = os.path.abspath(os.path.dirname(__file__))
+subjectCodeUrl = "https://www.shanghairanking.cn/api/pub/v1/bcsr/subj?year=2022"
+url='https://www.shanghairanking.cn/api/pub/v1/bcsr/rank?target_yr=2022&subj_code={}'
+
+def get_url(url):
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Referer':'https://shanghairanking.com/',
+        'DNT':'1',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Connection':'keep-alive',
+        'Accept-Language':'zh-CN,zh;q=0.9,en-CN;q=0.8,en;q=0.7',
+        'Accept-Encoding':'gzip, deflate',
+    }
+    req = requests.get(url, headers=headers)
+    req.encoding='utf-8'
+    html = req.json()
+    return html
+
+
+subjectCodeset = get_url(subjectCodeUrl)['data']
+# exit(0)
+with open(dirpath + "/bcsr.json", "w", encoding = 'utf-8') as f:
+    dict = []
+    for subject in subjectCodeset:
+        for item in subject["subjs"]:
+            dataset = get_url(url.format(item["code"]))['data']['rankings']
+            idx = len(dataset)
+            for i in range(idx):
+                Rank2022 = dataset[i]['ranking']
+                rankPctTop = dataset[i]['rankPctTop']
+                univNameCn = dataset[i]['univNameCn']
+                score = dataset[i]['score']
+                dict.append({"2022排名": Rank2022,
+                            "全部层次": rankPctTop,
+                            "学校": univNameCn,
+                            "总分": score,
+                            "学科门类": subject["nameCn"],
+                            "学科": item["nameCn"]})
+    json.dump(dict, f, ensure_ascii=False, indent=4)
+    f.close()
