c02811eb1783f36b179800ead6e79882146b488f
yym68686 <yym68686@outlook.com>
2023-04-24 14:51:27 +0800

    ✅南航智能科学与技术导师数据总结

--- nuaa/subject/main.py
+++ nuaa/subject/main.py
@@ -1,7 +1,9 @@
 import re
 import os
+os.system("clear")
 import json
 import pandas as pd
+from collections import Counter
 path = os.path.abspath(os.path.dirname(__file__)) + '/'
 
 teather = pd.read_excel(path + 'nuaa_supervisor_information.xlsm')
@@ -21,20 +23,33 @@ teather_de_dict = dict(zip(teather_de_dict.keys(), teather_de_dict.values()))
 # 专利
 patent = pd.read_excel(path + 'patent.xlsx', sheet_name=1)
 patent_list = patent['发明人（合并）'].tolist()
-patent_list = list(set([i for j in [i.split('; ') for i in patent_list] for i in j]))
+all_patent_list = [i for j in [i.split('; ') for i in patent_list] for i in j]
+patent_num_list = dict(Counter(all_patent_list))
+first_patent_list = [i.split('; ')[0] for i in patent_list]
+first_patent_num_list = dict(Counter(first_patent_list))
+patent_list = list(set(all_patent_list))
 
 # 论文
 article = pd.read_excel(path + 'article.xlsx', sheet_name=3)
 article_list = article['论文'].tolist()
 article_list = list(set([i for j in [re.findall(r'\[(.*?)\]', i) for i in article_list] for i in j]))
-article_list = list(set([i for j in [i.split('; ') for i in article_list] for i in j]))
+all_article_list = [i for j in [i.split('; ') for i in article_list] for i in j]
+article_num_list = dict(Counter(all_article_list))
+first_article_list = [i.split('; ')[0] for i in article_list]
+first_article_num_list = dict(Counter(first_article_list))
+article_list = list(set(all_article_list))
 
 # 研究课题
 subject = pd.read_excel(path + 'research_project.xlsx')
 subject_list = subject['项目主持人'].tolist() + subject['参与者'].tolist()
 subject_list = [i for i in subject_list if str(i) != 'nan']
-subject_list = list(set([i for j in [i.split('；') for j in [i.split('; ') for i in subject_list] for i in j] for i in j]))
+subject_num_list = [i for j in [i.split('；') for j in [i.split('; ') for i in subject_list] for i in j] for i in j]
+subject_list = list(set(subject_num_list))
 subject_list = [i for i in subject_list if str(i) != '']
+subject_num_list = dict(Counter(subject_num_list))
+first_subject = subject['项目主持人'].tolist()
+first_subject = [i for i in first_subject if str(i) != 'nan']
+first_subject_num_list = dict(Counter(first_subject))
 
 teather_list = list(set(patent_list + article_list + subject_list))
 
@@ -42,9 +57,9 @@ with open(path + 'teather.json', 'w') as f:
     result = []
     for name in teather_list:
         if name in teather_en2zh_dict.keys():
-            result.append({"英文名": name, "中文名": teather_en2zh_dict[name], "学院": teather_de_dict[teather_en2zh_dict[name]]})
+            result.append({"英文名": name, "中文名": teather_en2zh_dict[name], "学院": teather_de_dict[teather_en2zh_dict[name]], "发明专利个数": patent_num_list.get(teather_en2zh_dict[name], 0), "第一发明人个数": first_patent_num_list.get(teather_en2zh_dict[name], 0), "论文个数": article_num_list.get(name, 0), "第一作者论文个数": first_article_num_list.get(name, 0), "研究课题个数": subject_num_list.get(teather_en2zh_dict[name], 0), "主持研究课题个数": first_subject_num_list.get(teather_en2zh_dict[name], 0)})
         elif name in teather_zh2en_dict.keys():
-            result.append({"英文名": teather_zh2en_dict[name], "中文名": name, "学院": teather_de_dict[name]})
+            result.append({"英文名": teather_zh2en_dict[name], "中文名": name, "学院": teather_de_dict[name], "发明专利个数": patent_num_list.get(name, 0), "第一发明人个数": first_patent_num_list.get(name, 0), "论文个数": article_num_list.get(teather_zh2en_dict[name], 0), "第一作者论文个数": first_article_num_list.get(teather_zh2en_dict[name], 0), "研究课题个数": subject_num_list.get(name, 0), "主持研究课题个数": first_subject_num_list.get(name, 0)})
         else:
             print(name, 'not found')
     result = [dict(t) for t in set([tuple(d.items()) for d in result])]
